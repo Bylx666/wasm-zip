f2d633a5992936f7cdfd20f730258ecb3d78f67f
subkey <2822448396@qq.com>
2024-04-01 05:59:41 +0800

    fix: 后缀怎么没了

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ fn main() {
   let mut args = std::env::args_os();
   args.next();
   let path = args.next().expect("需要传入文件路径");
+
+  // 神奇的库, 只要导入再导出就能减少80%的文件体积
   let mut m = walrus::Module::from_file(&path).expect("找不到该文件");
+
   let mut out_name = std::path::PathBuf::from(path);
   let mut name = out_name.file_stem().expect("文件后缀需要是wasm").to_os_string();
   name.push("_opt");
   out_name.set_file_name(name);
+  out_name.push(".wasm");
+
   let _ = m.emit_wasm_file(out_name);
 }
